569180d254fd25888570cfe18a2481d3c3dac622
Patrick Johnson <pjpuzzler@gmail.com>
2021-12-31 23:53:46 -0500

    c

--- __init__.py
+++ __init__.py
@@ -27,6 +27,10 @@ HEX_COORDS = [(0, 0), (1, 0), (2, 0), (3, 1), (4, 2), (4, 3), (4, 4), (3, 4), (2
               (1, 3), (0, 2), (0, 1), (1, 1), (2, 1), (3, 2), (3, 3), (2, 3), (1, 2), (2, 2)]
 
 
+def vertex_from_hex_coords(x: int, y: int) -> Tuple[int, int]:
+    return 2 * x + abs(2 - y), y
+
+
 @dataclass
 class Building:
     is_city: bool
@@ -58,5 +62,5 @@ class CatanBoard:
         self.harbors = HARBORS[:]
         shuffle(self.harbors)
 
-        self.vertices = ...
+        self.vertices = [[None] * 11] * 6
         self.edges = ...
